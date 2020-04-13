ece6191cac620a5fc15ba764a491e39bd72ba1a0
erdemozturkish <62300448+erdemozturkish@users.noreply.github.com>
2020-04-12 23:42:31 -0400

    commit 6

--- src/test/java/NextClass.java
+++ src/test/java/NextClass.java
@@ -2,5 +2,6 @@ public class NextClass {
 
     public static void main(String[] args) {
         System.out.println("5th commit line");
+        System.out.println("This line was written on github.com with 'Edit' feature");
     }
 }
