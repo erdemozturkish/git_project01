81ef2b73aa78dfde273aecf11d9efd302e73a585
Erdem Ozturk <erdemozturkish@gmail.com>
2020-04-13 00:10:32 -0400

    7th commit

--- src/test/java/NextClass.java
+++ src/test/java/NextClass.java
@@ -3,5 +3,6 @@ public class NextClass {
     public static void main(String[] args) {
         System.out.println("5th commit line");
         System.out.println("This line was written on github.com with 'Edit' feature");
+        System.out.println("This line was written on intellij on 7th commit");
     }
 }
